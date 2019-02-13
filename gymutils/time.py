58a3271286df41cb86bb788f9a25403829c2a0f6
walkingmask <walkingmask@gmail.com>
2019-02-13 09:26:39 +0900

    Add get_now_str

--- gymutils/time.py
+++ gymutils/time.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import time
+
 
 class Timer:
     def __init__(self):
@@ -7,3 +9,15 @@ class Timer:
         self.start = time.time()
     def get(self):
         return time.strftime("%H:%M:%S", time.gmtime(time.time() - self.start))
+
+
+def get_now_str(year=True, month=True, day=True,
+        hour=True, minute=True, second=False, micro=False):
+    want = "%Y" if year else ""
+    want += "%m" if month else ""
+    want += "%d" if day else ""
+    want += "%H" if hour else ""
+    want += "%M" if minute else ""
+    want += "%S" if second else ""
+    want += "%f" if micro else ""
+    return datetime.now().strftime(want)
